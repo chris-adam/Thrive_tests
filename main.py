6e60776ad37d8de61058e2a57c3fa12e3a2a271b
chris-adam <31113062+chris-adam@users.noreply.github.com>
2020-06-30 22:53:49 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-""""""
-
-# Working with this google sheet:
-# https://docs.google.com/spreadsheets/d/1N7HGkyCrN2DA5ZhZNdCsEuxcBBWIb5_c7nrDHtFTUbQ/edit#gid=1822977730
-
-# if you want to have your own google sheet, follow this tutorial and replace client_secret.json with yours
-# https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
-
-# The sheet was designed only for slight changes. There might be bugs if you change the sheet too much.
-
-# Don't forget to change DEST_PATH to your Thrive/simulation_parameters folder below
-
-
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+# Path to the files to update
 DEST_PATH = "C:/Users/Chris/Desktop/Thrive/simulation_parameters/"
 
 
 def flatten_lst(lst):
+    """Transform a multidimensionnal list to a 1D list, filter empty values and convert str to float or int"""
     flat_list = []
     for sublist in lst:
         for item in sublist:
@@ -29,22 +18,36 @@ def flatten_lst(lst):
 
 
 def update_files(client, file_name, sheet_name=None, dest_path=DEST_PATH, extension=".json"):
+    """
+    Retrieve data from Google sheet and update Thrive files
+    :param client: Google sheet client
+    :param file_name: file to update
+    :param sheet_name: sheet where to retrive data
+    :param dest_path: where to write updated file
+    :param extension: extension of the final file
+    :return: None
+    """
     if sheet_name is None:
         sheet_name = file_name
 
+    # Retrieve data
     sheet = client.open_by_key("1N7HGkyCrN2DA5ZhZNdCsEuxcBBWIb5_c7nrDHtFTUbQ").worksheet(sheet_name)
     values = flatten_lst(sheet.get(file_name+"_values"))
 
+    # Read template file and update values
     with open("parameters_files/"+file_name+"_copy.txt") as file_r:
         text = file_r.read().format(*values).replace("~", "{").replace("@", "}")
+    # Write file in the dest_path folder
     with open(dest_path+file_name+extension, "w") as file_w:
         file_w.write(text)
 
 
+# Connect to google sheet
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
+# Update files
 for file in ("bio_processes", "biomes", "membranes", "organelles"):
     update_files(client, file, dest_path=DEST_PATH+"microbe_stage/")
 update_files(client, "Constants", "bio_processes", extension=".cs")
